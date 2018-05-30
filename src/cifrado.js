5ead7c0eb0a68103404b1b59ddb5e13ca6e2eeaa
fernanda vilches <andreaperez.gz@gmail.com>
2018-05-30 18:16:49 -0400

    se agrega html, se modifica cifrado, y se agrega style

---
+++ src/cifrado.js
@@ -0,0 +1,44 @@
+function cipher(){
+    
+    const cajaCcipher = document.getElementById('caja-cipher');
+    let propmt = prompt('pon una frase o palabra sin espacios');
+    let cifradas = ''; 
+    
+    for(var f = 0; f < propmt.length; f++){
+// var f=0 , la f va a representar cada letra de la palabra y empieza en 0
+//propmt.length , es el largo de las palabras
+//f++ , suma de a 1
+
+      let formula = (propmt.toUpperCase().charCodeAt(f) - 65 + 33) % 26 + 65;
+//-65 primera letra del abcdario / 33 valor fijo / 26 total letras del abcdario / +65 es el ASCII letra cifrada
+//charcodeat ubica la posicion de la letra en numero
+      let cifrado = String.fromCharCode(formula);
+      
+      cifradas+= cifrado; 
+      
+    }
+    cajaCcipher.innerHTML = cifradas; 
+  
+   
+  document.write(" Tu frase cifrada es " + "<br>" + cifradas);
+}
+
+function decipher(){
+    
+    const cajaDecipher = document.getElementById('caja-decipher');
+    let propmt = prompt('pon una palabra o frase sin espacio');
+    let deCcipher = '';
+  
+    for(var v = 0; v < propmt.length; v++){
+      
+        let formu = (propmt.toUpperCase().charCodeAt(v) + 65 - 33) % 26 + 65;
+      
+        let decifradas = String.fromCharCode(formu);
+        
+        deCcipher+= decifradas;
+    }
+    cajaDecipher.innerHTML = deCcipher;
+    
+    document.write(" Tu frase descifrada es " + "<br>" + deCcipher);
+}
+ 
