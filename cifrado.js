dda937a8462b185cf8171602c0b417683cea93af
fernanda vilches <andreaperez.gz@gmail.com>
2018-06-01 15:37:07 -0400

    no hay cambios

--- cifrado.js
+++ cifrado.js
@@ -1,17 +1,17 @@
 function cipher(){
     
     const cajaCcipher = document.getElementById('caja-cipher');
-    let propmt = prompt('pon una frase o palabra sin espacios');
+    let propmt = prompt('pon una palabra o frase sin espacios');
     let cifradas = ''; 
     
     for(var f = 0; f < propmt.length; f++){
-// var f=0 , la f va a representar cada letra de la palabra y empieza en 0
-//propmt.length , es el largo de las palabras
-//f++ , suma de a 1
+    // var f=0 , la f va a representar cada letra de la palabra y empieza en 0
+    //propmt.length , es el largo de las palabras
+    //f++ , suma de a 1
 
       let formula = (propmt.toUpperCase().charCodeAt(f) - 65 + 33) % 26 + 65;
-//-65 primera letra del abcdario / 33 valor fijo / 26 total letras del abcdario / +65 es el ASCII letra cifrada
-//charcodeat ubica la posicion de la letra en numero
+    //-65 primera letra del abcdario / 33 valor fijo / 26 total letras del abcdario / +65 es el ASCII letra cifrada
+    //charcodeat ubica la posicion de la letra en numero
       let cifrado = String.fromCharCode(formula);
       
       cifradas+= cifrado; 
@@ -20,18 +20,18 @@ function cipher(){
     cajaCcipher.innerHTML = cifradas; 
   
    
-  document.write(" Tu frase cifrada es " + "<br>" + cifradas);
+  
 }
 
 function decipher(){
     
     const cajaDecipher = document.getElementById('caja-decipher');
-    let propmt = prompt('pon una palabra o frase sin espacio');
+    let caja = prompt('pon una palabra o frase sin espacio');
     let deCcipher = '';
   
-    for(var v = 0; v < propmt.length; v++){
+    for(var v = 0; v < caja.length; v++){
       
-        let formu = (propmt.toUpperCase().charCodeAt(v) + 65 - 33) % 26 + 65;
+        let formu = (caja.toUpperCase().charCodeAt(v) + 65 - 33) % 26 + 65;
       
         let decifradas = String.fromCharCode(formu);
         
@@ -39,6 +39,12 @@ function decipher(){
     }
     cajaDecipher.innerHTML = deCcipher;
     
-    document.write(" Tu frase descifrada es " + "<br>" + deCcipher);
+    
 }
+ 
+
+
+
+
+
  
